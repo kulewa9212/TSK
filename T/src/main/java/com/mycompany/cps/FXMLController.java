892c17e4fb4a5ee02cf6969833a1f9ae2fe48d2c
ezkulew <ewa.kulesza92@wp.pl>
2018-11-18 21:30:57 +0100

    Prepare initial version for startCheck.

--- T/src/main/java/com/mycompany/cps/FXMLController.java
+++ T/src/main/java/com/mycompany/cps/FXMLController.java
@@ -1,105 +1,53 @@
 package com.mycompany.cps;
 
-
-
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.ScatterChart;
-import javafx.scene.control.ComboBox;
-
+import javafx.scene.control.TextField;
 
 public class FXMLController implements Initializable {
-
  
-
-   
-   
-    @FXML
-    private ComboBox Start;
-
-    @FXML
-    private ComboBox Koniec;
-
-    @FXML
-    private ComboBox A;
-
-    @FXML
-    private ComboBox Podzial;
-
-    @FXML
-    private ComboBox Rodzaj;
-
- 
-    @FXML
-    private ComboBox s1Rodzaj;
-
-    @FXML
-    private ComboBox s1Okres;
-
-    @FXML
-    private ComboBox s1Amplituda;
-
-    @FXML
-    private ComboBox s1Wypełnienie;
-
-    @FXML
-    private ComboBox s2Rodzaj;
-
-    @FXML
-    private ComboBox s2Okres;
-
-    @FXML
-    private ComboBox s2Amplituda;
-
-    @FXML
-    private ComboBox s2Wypełnienie;
-
-
-    @FXML
-    private ScatterChart<Number, Number> wykresDzialania;
-
     @FXML
     private ScatterChart<Number, Number> wykresDyskretny;
 
+    @FXML
+    private TextField massField;
 
+    @FXML
+    private TextField strengthtField;
 
-   
-   
+    @FXML
+    private TextField angleField;
 
-    public void cleanDzialanie() {
-        s1Amplituda.setValue(0.0);
-        s2Amplituda.setValue(0.0);
-        s1Okres.setValue(0.0);
-        s2Okres.setValue(0.0);
-        s1Rodzaj.setValue("");
-        s2Rodzaj.setValue("");
-        s1Wypełnienie.setValue(0.0);
-        s2Wypełnienie.setValue(0.0);
-        wykresDzialania.getData().clear();
+    @FXML
+    private TextField result;
 
+    @FXML
+    public void checkStart() {
+        doCheckStart(Double.parseDouble(massField.getText()),
+                Double.parseDouble(strengthtField.getText()),
+                Double.parseDouble(angleField.getText()));
     }
 
-  
-    private final ObservableList<Double> startValues = FXCollections.observableArrayList(0.0, 100.0, 1000.0, 2000.0);
-    private final ObservableList<Double> koniecValues = FXCollections.observableArrayList(1000.0, 5000.0, 10000.0, 20000.0);
-    private final ObservableList<Double> aValues = FXCollections.observableArrayList(1013.25);
-    private final ObservableList<String> iloscProbek = FXCollections.observableArrayList("2", "3", "5", "10", "20", "50", "100", "200", "500", "1000", "sygnał ciągły");
-    private final ObservableList<String> rodzajSygnalu = FXCollections.observableArrayList("wzór barometryczny");
-   
+    public void doCheckStart(double Fg, double Fn, double angle) {
+        double Fw = Math.sqrt(Fg * Fg + Fn * Fn
+                - 2 * Fg * Fn * Math.cos(Math.toRadians(angle)));
+        double beta = Math.toDegrees(Math.asin(
+                Math.sin(Math.toRadians(90 - angle)) * (Fg / Fw)));
+        if (beta < angle) {
+            result.setText("SUCCESS!");
+        } else {
+            result.setText("FAILURE!");
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Start.setItems(startValues);
-        Koniec.setItems(koniecValues);
-        A.setItems(aValues);
-        Podzial.setItems(iloscProbek);
-        Rodzaj.setItems(rodzajSygnalu);
+
         wykresDyskretny.setTitle("Prognoza ciśnienia");
         wykresDyskretny.setLegendVisible(true);
-       wykresDyskretny.getXAxis().setAutoRanging(true);
+        wykresDyskretny.getXAxis().setAutoRanging(true);
     }
 }
